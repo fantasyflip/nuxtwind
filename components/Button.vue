a4e319636a007162b327ae99d0787e6bd19a0b61
FantasyFlip <katzenberger.ph+github@gmail.com>
2022-11-30 15:42:38 +0100

    Started working on dynamically importing components depending on route

---
+++ components/Button.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>Button</div>
+</template>
+
+<script setup></script>
+
+<style scoped></style>
---
+++ pages/[locale]/component/[componentName].vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <component :is="selectedComponent" />
+  </div>
+</template>
+
+<script setup>
+
+import selectedComponent from ()=>{
+    const { params } = useRoute();
+    const selectedComponent = params.componentName;
+    let componentPath = `~/components/${selectedComponent}.vue`;
+    return componentPath
+};
+</script>
