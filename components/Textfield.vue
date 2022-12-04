1f701e1cf6b520915d223adea86ccc317caaf805
BLUEBEAST\katze <katzenberger.ph@gmail.com>
2022-12-04 03:58:23 +0100

    Started working on textfield component

---
+++ components/Textfield.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- <input
+    class="appearance-none rounded-md p-2 focus:outline-none focus:shadow-outline bg-zinc-800 focus:ring-2 ring-red-500"
+    type="text"
+    id="username"
+    placeholder="Placeholder"
+  /> -->
+  <!-- <input
+    class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+    id="username"
+    type="text"
+    placeholder="Username"
+  /> -->
+  <div class="py-6"></div>
+  <div class="w-56 relative group">
+    <input
+      type="text"
+      id="username"
+      required
+      class="w-full bg-zinc-800 h-10 px-4 text-sm peer outline-none focus:ring-2 ring-red-500 appearance-none rounded-md"
+    />
+    <label
+      for="username"
+      class="transform transition-all ease-in-out absolute top-0 left-0 h-full flex items-center pl-2 text-sm group-focus-within:text-xs peer-valid:text-xs group-focus-within:pb-2 group-focus-within:h-1/2 peer-valid:h-1/2 group-focus-within:-translate-y-full peer-valid:-translate-y-full cursor-text"
+      >Username</label
+    >
+  </div>
+</template>
+
+<script setup></script>
---
+++ pages/[locale]/component/textfield.vue
@@ -0,0 +1,154 @@
+<template>
+  <div>
+    <NuxtLayout name="component">
+      <template #componentName> Textfield </template>
+      <template #component>
+        <Textfield />
+      </template>
+      <template #playground> Playground </template>
+      <template #props>
+        <div
+          v-for="(prop, index) in props"
+          :class="index < props.length - 1 ? 'border-b' : ''"
+        >
+          <PropDisplay :item="prop" />
+        </div>
+      </template>
+      <template #propertyCount>{{ props.length }}</template>
+    </NuxtLayout>
+  </div>
+</template>
+<script setup>
+import { useI18n } from "vue-i18n";
+const { t } = useI18n();
+let props = [
+  {
+    name: "color",
+    type: "Object",
+    default: {
+      bg: "bg-cyan-900",
+      text: "text-white",
+      border: "border-white",
+      hover: "hover:bg-cyan-800",
+    },
+    description: t(
+      "pages.component.button.content.properties.color.description"
+    ),
+  },
+  {
+    name: "borderRadius",
+    type: "Boolean, String",
+    default: "rounded-md",
+    description: t(
+      "pages.component.button.content.properties.borderRadius.description"
+    ),
+  },
+  {
+    name: "grow",
+    type: "Boolean, Object",
+    default: {
+      delay: "delay-10",
+      scale: "hover:scale-110",
+    },
+    description: t(
+      "pages.component.button.content.properties.grow.description"
+    ),
+  },
+  {
+    name: "link",
+    type: "Object, String",
+    default: undefined,
+    description: t(
+      "pages.component.button.content.properties.link.description"
+    ),
+  },
+  {
+    name: "target",
+    type: "String",
+    default: "_self",
+    description: t(
+      "pages.component.button.content.properties.target.description"
+    ),
+  },
+  {
+    name: "disabled",
+    type: "Boolean",
+    default: false,
+    description: t(
+      "pages.component.button.content.properties.disabled.description"
+    ),
+  },
+  {
+    name: "icon",
+    type: "Boolean",
+    default: false,
+    description: t(
+      "pages.component.button.content.properties.icon.description"
+    ),
+  },
+  {
+    name: "loading",
+    type: "Boolean",
+    default: false,
+    description: t(
+      "pages.component.button.content.properties.loading.description"
+    ),
+  },
+  {
+    name: "outlined",
+    type: "Boolean",
+    default: "border-2",
+    description: t(
+      "pages.component.button.content.properties.outlined.description"
+    ),
+  },
+  {
+    name: "shadow",
+    type: "Boolean",
+    default: "shadow-md",
+    description: t(
+      "pages.component.button.content.properties.shadow.description"
+    ),
+  },
+  {
+    name: "transition",
+    type: "Object, Boolean",
+    default: {
+      duration: "duration-300",
+      ease: "ease-in-out",
+    },
+    description: t(
+      "pages.component.button.content.properties.transition.description"
+    ),
+  },
+  {
+    name: "dense",
+    type: "Boolean",
+    default: false,
+    description: t(
+      "pages.component.button.content.properties.dense.description"
+    ),
+  },
+  {
+    name: "width",
+    type: "String",
+    default: undefined,
+    description: t(
+      "pages.component.button.content.properties.width.description"
+    ),
+  },
+  {
+    name: "height",
+    type: "String",
+    default: undefined,
+    description: t(
+      "pages.component.button.content.properties.height.description"
+    ),
+  },
+];
+
+definePageMeta({
+  layout: false,
+});
+</script>
+<style scoped></style>
