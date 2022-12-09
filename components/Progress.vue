a4f5561a4b876da07e722076df6fac3215cf29db
BLUEBEAST\katze <katzenberger.ph@gmail.com>
2022-12-09 19:12:47 +0100

    Worked on progress component and documentation

--- components/Progress.vue
+++ components/Progress.vue
@@ -1,25 +1,22 @@
 <template>
-  <div v-if="props.circular" role="status">
-    <svg
-      aria-hidden="true"
-      :class="styleClass"
-      viewBox="0 0 100 101"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <path
-        d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
-        fill="currentColor"
-      />
-      <path
-        d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
-        fill="currentFill"
-      />
-    </svg>
-    <span class="sr-only">Loading...</span>
+  <div
+    id="circular-progress"
+    v-if="props.circular"
+    :style="circularStyle"
+    :class="circularStyleClass"
+  >
+    <div class="relative w-full h-full">
+      <slot name="inner-circle">
+        <div
+          class="h-full w-full grid place-items-center"
+          :class="props.circular.cutout?.text || defaults.circular.cutout.text"
+        >
+          {{ progressValue }}%
+        </div>
+      </slot>
+    </div>
   </div>
-
-  <div v-else :class="styleClass">
+  <div v-else :class="linearWrapperStyleClass">
     <div :class="linearStyleClass" :style="linearProgressStyle" />
   </div>
 </template>
@@ -33,10 +30,16 @@ let defaults = {
   circular: {
     width: "w-10",
     height: "h-10",
+    cutout: {
+      width: "before:w-[84%]",
+      height: "before:h-[84%]",
+      text: "text-[70%]",
+    },
   },
   color: {
-    fill: "fill-cyan-800",
-    circle: "text-zinc-800",
+    circle: "#27272a",
+    circleProgress: "#155e75",
+    circleCutout: "before:bg-zinc-900",
     background: "bg-zinc-800",
     firstStrike: "before:bg-cyan-800",
     secondStrike: "after:bg-cyan-600",
@@ -66,11 +69,14 @@ const props = defineProps({
   color: {
     type: Object,
     default: {
-      fill: "fill-cyan-800",
-      circle: "text-zinc-800",
+      circle: "#27272a",
+      circleProgress: "#155e75",
+      circleCutout: "before:bg-zinc-900",
       background: "bg-zinc-800",
       firstStrike: "before:bg-cyan-800",
       secondStrike: "after:bg-cyan-600",
+      linearProgress: "bg-cyan-800",
+      linearProgressHover: "hover:bg-emerald-700",
     },
   },
   circular: {
@@ -81,7 +87,7 @@ const props = defineProps({
     type: Boolean,
     default: false,
   },
-  initialLoadTimeout: {
+  initialLoadTime: {
     type: [Number, Boolean],
     default: 100,
   },
@@ -95,45 +101,101 @@ const props = defineProps({
   },
 });
 
-let loadTimeoutOver = ref(false);
+let initialLoadTime = ref(false);
 
-if (props.initialLoadTimeout) {
+if (props.initialLoadTime) {
   setTimeout(() => {
-    loadTimeoutOver.value = true;
-  }, props.initialLoadTimeout);
+    initialLoadTime.value = true;
+  }, props.initialLoadTime);
 }
 
-const styleClass = computed(() => {
+let progressValue = ref(0);
+let endValue = ref(0);
+
+if (props.modelValue === undefined && props.loading) {
+  endValue.value = 30;
+} else if (props.modelValue > 100) {
+  endValue.value = 100;
+} else if (props.modelValue < 0) {
+  endValue.value = 0;
+} else if (props.modelValue === undefined) {
+  endValue.value = 0;
+} else {
+  endValue.value = props.modelValue;
+}
+
+if (props.initialLoadTime && endValue.value > 0) {
+  let speed = props.initialLoadTime / endValue.value;
+
+  let progress = setInterval(() => {
+    progressValue.value++;
+    if (progressValue.value == endValue.value) {
+      clearInterval(progress);
+    }
+  }, speed);
+} else {
+  progressValue.value = endValue.value;
+}
+
+const circularStyle = computed(() => {
+  return `background: conic-gradient(
+      ${props.color.circleProgress || defaults.color.circleProgress} ${
+    progressValue.value * 3.6
+  }deg,
+      ${props.color.circle || defaults.color.circle} ${
+    progressValue.value * 3.6
+  }deg);`;
+});
+
+const circularStyleClass = computed(() => {
+  let classes = [];
+  // class="relative w-[250px] h-[250px] rounded-[50%] grid place-items-center before:content-[''] before:absolute before:h-[84%] before:w-[84%] before:bg-zinc-900 before:rounded-[50%]"
+  classes.push(
+    "relative",
+    "rounded-[50%]",
+    "grid",
+    "place-items-center",
+    "before:content-['']",
+    "before:absolute",
+    "before:rounded-[50%]"
+  );
+
+  //SIZE
+  classes.push(props.circular.width || defaults.circular.width);
+  classes.push(props.circular.height || defaults.circular.height);
+  classes.push(props.circular.cutout?.width || defaults.circular.cutout.width);
+  classes.push(
+    props.circular.cutout?.height || defaults.circular.cutout.height
+  );
+
+  //COLOR
+  classes.push(props.color.circleCutout || defaults.color.circleCutout);
+
+  //LOADING
+  if (props.loading) {
+    classes.push("animate-spin");
+  }
+
+  return classes.join(" ");
+});
+
+const linearWrapperStyleClass = computed(() => {
   let classes = [];
 
-  if (props.circular) {
-    if (props.loading) {
-      classes.push("animate-spin");
-    }
+  //COLOR
+  classes.push("overflow-hidden");
+  classes.push(props.color.background || defaults.color.background);
 
-    //COLOR
-    classes.push(props.color.circle || defaults.color.circle);
-    classes.push(props.color.fill || defaults.color.fill);
+  //SIZE
+  classes.push(props.size.width || defaults.size.width);
+  classes.push(props.size.height || defaults.size.height);
 
-    //SIZE
-    classes.push(props.circular.width || defaults.circular.width);
-    classes.push(props.circular.height || defaults.circular.height);
-  } else {
-    //COLOR
-    classes.push("overflow-hidden");
-    classes.push(props.color.background || defaults.color.background);
-
-    //SIZE
-    classes.push(props.size.width || defaults.size.width);
-    classes.push(props.size.height || defaults.size.height);
-
-    //ROUNDED
-    if (props.rounded) {
-      if (typeof props.rounded === "string") {
-        classes.push(props.rounded);
-      } else {
-        classes.push(defaults.rounded);
-      }
+  //ROUNDED
+  if (props.rounded) {
+    if (typeof props.rounded === "string") {
+      classes.push(props.rounded);
+    } else {
+      classes.push(defaults.rounded);
     }
   }
 
@@ -178,7 +240,7 @@ const linearStyleClass = computed(() => {
 });
 
 const linearProgressStyle = computed(() => {
-  if (loadTimeoutOver.value || !props.initialLoadTimeout) {
+  if (initialLoadTime.value || !props.initialLoadTime) {
     let percent = 0;
 
     let input = null;
--- pages/[locale]/component/progress.vue
+++ pages/[locale]/component/progress.vue
@@ -3,7 +3,7 @@
     <NuxtLayout name="component">
       <template #componentName> Progress </template>
       <template #component>
-        <Progress rounded loading />
+        <Progress rounded loading> </Progress>
       </template>
       <template #playground> Playground </template>
       <template #props>
@@ -39,11 +39,84 @@
 <script setup>
 import { useI18n } from "vue-i18n";
 const { t } = useI18n();
-let props = [];
+let props = [
+  {
+    name: "size",
+    type: "Object",
+    default: {
+      width: "w-full",
+      height: "h-1",
+    },
+    description: t(
+      "pages.component.progress.content.properties.size.description"
+    ),
+  },
+  {
+    name: "color",
+    type: "Object",
+    default: {
+      circle: "#27272a",
+      circleProgress: "#155e75",
+      circleCutout: "before:bg-zinc-900",
+      background: "bg-zinc-800",
+      firstStrike: "before:bg-cyan-800",
+      secondStrike: "after:bg-cyan-600",
+      linearProgress: "bg-cyan-800",
+      linearProgressHover: "hover:bg-emerald-700",
+    },
+    description: t(
+      "pages.component.progress.content.properties.color.description"
+    ),
+  },
+  {
+    name: "circular",
+    type: "Boolean, Object",
+    default: false,
+    description: t(
+      "pages.component.progress.content.properties.circular.description"
+    ),
+  },
+  {
+    name: "loading",
+    type: "Boolean",
+    default: false,
+    description: t(
+      "pages.component.progress.content.properties.loading.description"
+    ),
+  },
+  {
+    name: "initialLoadTime",
+    type: "Number, Boolean",
+    default: 100,
+    description: t(
+      "pages.component.progress.content.properties.initialLoadTime.description"
+    ),
+  },
+  {
+    name: "transition",
+    type: "Boolean, Object",
+    default: true,
+  },
+  {
+    name: "rounded",
+    type: "Boolean, String",
+    default: true,
+    description: t(
+      "pages.component.progress.content.properties.rounded.description"
+    ),
+  },
+];
 
 let events = [];
 
-let slots = [];
+let slots = [
+  {
+    name: "inner-circle",
+    description: t(
+      "pages.component.progress.content.slots.innerCircle.description"
+    ),
+  },
+];
 
 definePageMeta({
   layout: false,
