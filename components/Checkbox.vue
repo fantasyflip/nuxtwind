61f85205a26d5dbf0890a807c02928df44f6a5a7
FantasyFlip <katzenberger.ph+github@gmail.com>
2022-12-14 10:20:28 +0100

    Added checkbox component

---
+++ components/Checkbox.vue
@@ -0,0 +1,138 @@
+<template>
+  <div class="flex">
+    <div :class="iconStyleClass" id="checkbox">
+      <MdiCheckboxMarked
+        v-if="modelValue"
+        @click="$emit('update:modelValue', false)"
+      />
+      <MdiCheckboxBlankOutline
+        v-else
+        @click="$emit('update:modelValue', true)"
+      />
+    </div>
+    <div v-if="props.label" :class="labelStyleClass">
+      <label for="checkbox">
+        <slot name="label">{{ label }}</slot>
+      </label>
+      <p
+        v-if="description"
+        id="checkbox-description"
+        :class="descriptionStyleClass"
+      >
+        <slot name="description">{{ description }}</slot>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import MdiCheckboxBlankOutline from "~icons/mdi/checkbox-blank-outline";
+import MdiCheckboxMarked from "~icons/mdi/checkbox-marked";
+
+let defaults = {
+  color: {
+    label: "text-black dark:text-gray-300",
+    description: "text-gray-500 dark:text-gray-400",
+    iconInactive: "text-gray-500 dark:text-gray-400",
+    iconActive: "text-primary-800 dark:text-primary-800",
+    hover: "hover:text-secondary-700 dark:hover:text-secondary-700",
+  },
+  text: {
+    label: "text-sm font-medium",
+    description: "text-xs font-normal",
+  },
+};
+
+let props = defineProps({
+  modelValue: {
+    type: Boolean,
+    default: false,
+  },
+  color: {
+    type: Object,
+    default: {
+      label: "text-black dark:text-gray-300",
+      description: "text-gray-500 dark:text-gray-400",
+      iconInactive: "text-gray-500 dark:text-gray-400",
+      iconActive: "text-primary-800 dark:text-primary-800",
+      hover: "hover:text-secondary-700 dark:hover:text-secondary-700",
+    },
+  },
+  label: {
+    type: String,
+    default: "",
+  },
+  description: {
+    type: String,
+    default: "",
+  },
+  text: {
+    type: Object,
+    default: {
+      label: "text-sm font-medium",
+      description: "text-xs font-normal",
+    },
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+  loading: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+let emit = defineEmits(["update:modelValue"]);
+
+let iconStyleClass = computed(() => {
+  let classes = [];
+  //   class="flex items-center h-5"
+  classes.push("flex", "items-center", "h-5");
+
+  //COLOR
+
+  if (props.modelValue) {
+    classes.push(props.color?.iconActive || defaults.color.iconActive);
+  } else {
+    classes.push(props.color?.iconInactive || defaults.color.iconInactive);
+  }
+
+  //DISABLED && LOADING
+  if (props.disabled) {
+    classes.push("cursor-not-allowed");
+    classes.push("opacity-50");
+  } else if (props.loading) {
+    classes.push("cursor-wait");
+    classes.push("animate-pulse");
+  } else {
+    classes.push("cursor-pointer");
+    classes.push(props.color?.hover || defaults.color.hover);
+  }
+
+  return classes.join(" ");
+});
+
+let labelStyleClass = computed(() => {
+  let classes = [];
+  //   class="ml-3 text-sm font-medium text-gray-700"
+  classes.push("ml-2");
+
+  classes.push(props.text?.label || defaults.text.label);
+
+  classes.push(props.color?.label || defaults.color.label);
+
+  return classes.join(" ");
+});
+
+let descriptionStyleClass = computed(() => {
+  let classes = [];
+  //   class="text-sm text-gray-500"
+
+  classes.push(props.text?.description || defaults.text.description);
+
+  classes.push(props.color?.description || defaults.color.description);
+
+  return classes.join(" ");
+});
+</script>
---
+++ pages/[locale]/component/checkbox.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <NuxtLayout name="component">
+      <template #componentName> {{ componentName }} </template>
+      <template #component>
+        <Checkbox
+          v-model="checkValue"
+          :label="checkValue ? 'Uncheck me!' : 'Check me!'"
+          description="This is a chechbox!"
+        />
+      </template>
+      <template #playground>
+        <div class="grid place-items-center h-full w-full">Playground</div>
+      </template>
+      <template #documentation>
+        <DocumentationDisplay
+          :componentName="componentName"
+          :props="props"
+          :slots="slots"
+          :caveats="caveats"
+        />
+      </template>
+    </NuxtLayout>
+  </div>
+</template>
+<script setup>
+import { useI18n } from "vue-i18n";
+const { t } = useI18n();
+
+//PLAYGROUND
+let checkValue = ref(true);
+
+//DOCUMENTATION
+let componentName = "Checkbox";
+
+let props = [];
+
+let slots = [];
+
+let caveats = [];
+
+definePageMeta({
+  layout: false,
+});
+</script>
+<style scoped></style>
