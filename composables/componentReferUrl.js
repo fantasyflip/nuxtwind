8ad90a84f951a7f8a13d98fd274948b7cfb30260
FantasyFlip <katzenberger.ph+github@gmail.com>
2022-12-19 09:05:28 +0100

    fix(docs): make referLink params modular

--- composables/componentReferUrl.js
+++ composables/componentReferUrl.js
@@ -1,5 +1,7 @@
 export const useComponentReferUrl = (component, documentationType, item) => {
   const route = useRoute();
-  return `/${route.params.locale}/component/${component}#${documentationType}-${item}`;
+  return `/${route.params.locale}/component/${component}${
+    documentationType ? "#" + documentationType + (item ? "-" + item : "") : ""
+  }`;
   //TODO add support for only component and documentationType
 };
