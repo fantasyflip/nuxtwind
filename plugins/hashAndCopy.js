f25746acb3ce798a8bc7f8ccd1aba2b7b3eb3008
FantasyFlip <katzenberger.ph+github@gmail.com>
2022-12-19 08:55:40 +0100

    feat(docs): toast on link copy

--- plugins/hashAndCopy.js
+++ plugins/hashAndCopy.js
@@ -1,12 +1,28 @@
+import MdiLinkVariant from "~icons/mdi/link-variant";
+
 export default defineNuxtPlugin((nuxtApp) => {
+  const linkIcon = markRaw(MdiLinkVariant);
   nuxtApp.provide("hashAndCopy", (id) => {
     const route = useRoute();
+    const currentLocale = route.params.locale;
+
     let path = route.path;
     let hash = "#" + id;
     let fullPath = path + hash;
     let newUrl = window.location.origin + fullPath;
     navigator.clipboard.writeText(newUrl);
     navigateTo(fullPath);
-    //TODO add notification on copy
+
+    useToast({
+      title:
+        currentLocale === "de"
+          ? "Erfolgreich kopiert!"
+          : "Successfully copied!",
+      message:
+        (currentLocale === "de" ? "Link kopiert zu: " : "Copied link to: ") +
+        capitalizeFirstLetter(id),
+      icon: linkIcon,
+      type: "success",
+    });
   });
 });
