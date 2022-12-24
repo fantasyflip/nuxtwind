8b416d68d93d9554d6cc5087bf858797675c9d84
BLUEBEAST\katze <katzenberger.ph@gmail.com>
2022-12-24 17:23:28 +0100

    feat(drawer): Create component

---
+++ components/Drawer.vue
@@ -0,0 +1,127 @@
+<template>
+  <div v-if="props.modelValue" :class="overlayClass"></div>
+  <div ref="drawer" :class="drawerWrapperClass"></div>
+</template>
+
+<script setup>
+let defaults = {
+  color: {
+    bg: "bg-white dark:bg-gray-800",
+    overlayBg: "bg-black/50",
+  },
+};
+
+let props = defineProps({
+  modelValue: {},
+  color: {
+    type: Object,
+    default: {
+      bg: "bg-white dark:bg-zinc-900",
+      overlayBg: "bg-black/50",
+    },
+  },
+  fixed: {
+    type: Boolean,
+    default: true,
+  },
+  zIndex: {
+    type: String,
+    default: "z-[100]",
+  },
+  overlay: {
+    type: Boolean,
+    default: true,
+  },
+  disableOverflow: {
+    type: Boolean,
+    default: true,
+  },
+  height: {
+    type: String,
+    default: "h-screen",
+  },
+  width: {
+    type: String,
+    default: "md:w-80 w-60",
+  },
+});
+
+let emit = defineEmits(["update:modelValue"]);
+
+let modelValueComputed = computed(() => {
+  return props.modelValue;
+});
+
+//watch modelValueComputed
+watch(modelValueComputed, (newVal, oldVal) => {
+  if (newVal) {
+    if (props.disableOverflow) {
+      document.body.classList.add("overflow-hidden");
+      document.getElementsByTagName("html")[0].style.overflow = "hidden";
+    }
+  }
+});
+
+const drawer = ref(null);
+
+onClickOutside(drawer, () => {
+  emit("update:modelValue", false);
+  if (props.disableOverflow) {
+    document.body.classList.remove("overflow-hidden");
+    document.getElementsByTagName("html")[0].style.overflow = "auto";
+  }
+});
+
+let overlayClass = computed(() => {
+  let classes = [];
+  classes.push(
+    "h-full",
+    "w-full",
+    "fixed",
+    "top-0",
+    "left-0",
+    "right-0",
+    "bottom-0",
+    "z-[99]",
+    "overflow-hidden"
+  );
+
+  if (props.overlay) {
+    classes.push(props.color?.overlayBg || defaults.color.overlayBg);
+  }
+
+  console.log(classes.join(" "));
+
+  return classes.join(" ");
+});
+
+let drawerWrapperClass = computed(() => {
+  let classes = [];
+
+  classes.push(
+    "overflow-y-auto",
+    "transition-all",
+    "duration-300",
+    "ease-in-out",
+    "transform"
+  );
+  if (props.modelValue) {
+    classes.push("translate-x-0");
+  } else {
+    classes.push("-translate-x-full");
+  }
+
+  classes.push(props.color?.bg || defaults.color.bg);
+
+  if (props.fixed) {
+    classes.push("fixed");
+  }
+
+  classes.push(props.zIndex);
+
+  classes.push(props.height);
+  classes.push(props.width);
+
+  return classes.join(" ");
+});
+</script>
