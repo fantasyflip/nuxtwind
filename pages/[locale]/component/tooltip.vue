c00bbfe3476f0b775743f15a77be7eadbdec79b2
BLUEBEAST\katze <katzenberger.ph@gmail.com>
2022-12-29 20:35:22 +0100

    feat(tooltip): Add working playground

--- pages/[locale]/component/tooltip.vue
+++ pages/[locale]/component/tooltip.vue
@@ -3,19 +3,64 @@
     <NuxtLayout name="component">
       <template #componentName> {{ componentName }} </template>
       <template #component>
-        <Tooltip />
+        <Tooltip
+          :top="checkedPositionBoxes[0]"
+          :bottom="checkedPositionBoxes[1]"
+          :right="checkedPositionBoxes[2]"
+          :left="checkedPositionBoxes[3]"
+          :rounded="rounded"
+        >
+          <Button
+            @click="
+              useToast({
+                title: '(╯°□°)╯︵ ┻━┻',
+                message: 'You´re supposed to hover over me and not click!',
+                autoClose: true,
+                type: 'warning',
+              })
+            "
+            >Hover over me!</Button
+          >
+
+          <template #tooltip>
+            {{ tooltip }}
+          </template>
+        </Tooltip>
       </template>
       <template #playground>
         <div class="grid place-items-center h-full w-full">
-          <div class="grid place-items-center sm:w-1/2 gap-3 py-2"></div>
+          <div class="grid place-items-center sm:w-1/2 gap-3 py-2">
+            <Textfield
+              v-model="tooltip"
+              class="mb-4"
+              label="Tooltip Text"
+              filled
+            />
+            <Checkboxgroup
+              v-model="checkedPositionBoxes"
+              not-zero
+              :items="postitionProps"
+              :label="
+                $t(
+                  'pages.component.tooltip.content.playground.checkboxgroup.label'
+                )
+              "
+              :description="
+                $t(
+                  'pages.component.tooltip.content.playground.checkboxgroup.description'
+                )
+              "
+            />
+            <div class="w-full">
+              <Checkbox v-model="rounded" label="Rounded" />
+            </div>
+          </div>
         </div>
       </template>
       <template #documentation>
         <DocumentationDisplay
           :componentName="componentName"
           :props="props"
-          :events="events"
-          :caveats="caveats"
           :slots="slots"
         />
       </template>
@@ -27,13 +72,30 @@ import { useI18n } from "vue-i18n";
 const { t } = useI18n();
 
 //PLAYGROUND
+let tooltip = ref("This is a tooltip telling you not to click the button! :)");
+let checkedPositionBoxes = ref([true, false, false, false]);
+
+let postitionProps = ref([
+  {
+    label: "Top",
+  },
+  {
+    label: "Bottom",
+  },
+  {
+    label: "Right",
+  },
+  {
+    label: "Left",
+  },
+]);
+
+let rounded = ref(true);
 
 //DOCUMENTATION
 let componentName = "Tooltip";
 
 let props = [];
-
-let events = [];
 
 let slots = [];
 
