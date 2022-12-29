68f9ba74d192c7ba2a0a7b4de7ee2714292e42cc
FantasyFlip <katzenberger.ph+github@gmail.com>
2022-12-29 15:16:16 +0100

    feat(tooltip): Create component and page

---
+++ pages/[locale]/component/tooltip.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <NuxtLayout name="component">
+      <template #componentName> {{ componentName }} </template>
+      <template #component>
+        <Tooltip />
+      </template>
+      <template #playground>
+        <div class="grid place-items-center h-full w-full">
+          <div class="grid place-items-center sm:w-1/2 gap-3 py-2"></div>
+        </div>
+      </template>
+      <template #documentation>
+        <DocumentationDisplay
+          :componentName="componentName"
+          :props="props"
+          :events="events"
+          :caveats="caveats"
+          :slots="slots"
+        />
+      </template>
+    </NuxtLayout>
+  </div>
+</template>
+<script setup>
+import { useI18n } from "vue-i18n";
+const { t } = useI18n();
+
+//PLAYGROUND
+
+//DOCUMENTATION
+let componentName = "Tooltip";
+
+let props = [];
+
+let events = [];
+
+let slots = [];
+
+definePageMeta({
+  layout: false,
+});
+</script>
+<style scoped></style>
