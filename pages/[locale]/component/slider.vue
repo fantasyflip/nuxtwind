7b207a1f21df249377c884f9e9472cc73aefba60
FantasyFlip <katzenberger.ph@gmail.com>
2023-01-11 19:07:16 +0100

    feat(slider): Add documentation

--- pages/[locale]/component/slider.vue
+++ pages/[locale]/component/slider.vue
@@ -42,7 +42,11 @@
         </div>
       </template>
       <template #documentation>
-        <DocumentationDisplay :component-name="componentName" :props="props" />
+        <DocumentationDisplay
+          :component-name="componentName"
+          :props="props"
+          :slots="slots"
+        />
       </template>
     </NuxtLayout>
   </div>
@@ -64,7 +68,102 @@ let slider = ref({
 //DOCUMENTATION
 let componentName = "Slider";
 
-let props = [];
+let props = [
+  {
+    name: "color",
+    type: "Object",
+    default: {
+      description: "text-zinc-400",
+      sliderLine: "bg-zinc-600 dark:bg-zinc-600",
+      sliderThumb: "primary-500",
+      sliderThumbDark: "primary-500",
+      sliderThumbBorder: "primary-800",
+      sliderThumbBorderDark: "primary-800",
+    },
+    description: t(
+      "pages.component.slider.content.documentation.properties.color.description"
+    ),
+  },
+  {
+    name: "label",
+    type: "String",
+    default: "",
+    description: t(
+      "pages.component.slider.content.documentation.properties.label.description"
+    ),
+  },
+  {
+    name: "description",
+    type: "String",
+    default: "",
+    description: t(
+      "pages.component.slider.content.documentation.properties.description.description"
+    ),
+  },
+  {
+    name: "min",
+    type: "Number",
+    default: 0,
+    description: t(
+      "pages.component.slider.content.documentation.properties.min.description"
+    ),
+  },
+  {
+    name: "max",
+    type: "Number",
+    default: 100,
+    description: t(
+      "pages.component.slider.content.documentation.properties.max.description"
+    ),
+  },
+  {
+    name: "thumbSize",
+    type: "Number",
+    default: 15,
+    description: t(
+      "pages.component.slider.content.documentation.properties.thumbSize.description"
+    ),
+  },
+  {
+    name: "rounded",
+    type: "Boolean, String",
+    default: true,
+    description: t(
+      "pages.component.slider.content.documentation.properties.rounded.description"
+    ),
+  },
+  {
+    name: "width",
+    type: "String",
+    default: "w-full",
+    description: t(
+      "pages.component.slider.content.documentation.properties.width.description"
+    ),
+  },
+  {
+    name: "height",
+    type: "String",
+    default: "h-0.5",
+    description: t(
+      "pages.component.slider.content.documentation.properties.height.description"
+    ),
+  },
+];
+
+let slots = [
+  {
+    name: "label",
+    description: t(
+      "pages.component.slider.content.documentation.slots.label.description"
+    ),
+  },
+  {
+    name: "description",
+    description: t(
+      "pages.component.slider.content.documentation.slots.description.description"
+    ),
+  },
+];
 
 definePageMeta({
   layout: false,
