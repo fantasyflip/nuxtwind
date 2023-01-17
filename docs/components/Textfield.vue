e4034a8d118c35ee82f15f184fbbdf3fa9632119
FantasyFlip <katzenberger.ph@gmail.com>
2023-01-17 22:31:43 +0100

    chore(textfield): Move generateId function inside component

--- docs/components/Textfield.vue
+++ docs/components/Textfield.vue
@@ -43,6 +43,17 @@
 </template>
 
 <script setup>
+import { computed, ref } from "vue";
+function generateId() {
+  let result = "";
+  let characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  let charactersLength = characters.length;
+  for (let i = 0; i < 8; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
 let textfieldId = generateId();
 
 let isValid = ref(true);
