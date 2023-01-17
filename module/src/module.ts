08905167b9ce841f7364166b21696c406fec8346
FantasyFlip <katzenberger.ph@gmail.com>
2023-01-17 22:30:46 +0100

    feat(module): Add components to module

--- module/src/module.ts
+++ module/src/module.ts
@@ -21,20 +21,60 @@ export default defineNuxtModule({
     const componentsDir = reslover.resolve(runtimeDir, "components");
     const prefix = options.customPrefix || "NXW-";
     addComponent({
-      name: prefix + "Badge", // name of the component to be used in vue templates
-      filePath: resolve(componentsDir, "Badge.vue"), // resolve(runtimeDir, 'components', 'MyComponent.vue')
+      name: prefix + "Appbar", // name of the component to be used in vue templates
+      filePath: resolve(componentsDir, "Appbar.vue"), // resolve(runtimeDir, 'components', 'MyComponent.vue')
     });
     addComponent({
-      name: prefix + "Textfield", // name of the component to be used in vue templates
-      filePath: resolve(componentsDir, "Textfield.vue"), // resolve(runtimeDir, 'components', 'MyComponent.vue')
+      name: prefix + "Badge",
+      filePath: resolve(componentsDir, "Badge.vue"),
     });
     addComponent({
-      name: prefix + "Checkbox", // name of the component to be used in vue templates
-      filePath: resolve(componentsDir, "Checkbox.vue"), // resolve(runtimeDir, 'components', 'MyComponent.vue')
+      name: prefix + "Button",
+      filePath: resolve(componentsDir, "Button.vue"),
     });
     addComponent({
-      name: prefix + "Checkboxgroup", // name of the component to be used in vue templates
-      filePath: resolve(componentsDir, "Checkboxgroup.vue"), // resolve(runtimeDir, 'components', 'MyComponent.vue')
+      name: prefix + "Carousel",
+      filePath: resolve(componentsDir, "Carousel.vue"),
+    });
+    addComponent({
+      name: prefix + "CarouselItem",
+      filePath: resolve(componentsDir, "CarouselItem.vue"),
+    });
+    addComponent({
+      name: prefix + "Checkbox",
+      filePath: resolve(componentsDir, "Checkbox.vue"),
+    });
+    addComponent({
+      name: prefix + "Checkboxgroup",
+      filePath: resolve(componentsDir, "Checkboxgroup.vue"),
+    });
+    addComponent({
+      name: prefix + "Drawer",
+      filePath: resolve(componentsDir, "Drawer.vue"),
+    });
+    addComponent({
+      name: prefix + "Progress",
+      filePath: resolve(componentsDir, "Progress.vue"),
+    });
+    addComponent({
+      name: prefix + "Select",
+      filePath: resolve(componentsDir, "Select.vue"),
+    });
+    addComponent({
+      name: prefix + "Slider",
+      filePath: resolve(componentsDir, "Slider.vue"),
+    });
+    addComponent({
+      name: prefix + "Textfield",
+      filePath: resolve(componentsDir, "Textfield.vue"),
+    });
+    addComponent({
+      name: prefix + "Toast",
+      filePath: resolve(componentsDir, "Toast.vue"),
+    });
+    addComponent({
+      name: prefix + "Tooltip",
+      filePath: resolve(componentsDir, "Tooltip.vue"),
     });
   },
 });
