9d5d36eae96d5be3da3a8e81e1498451dad909e4
FantasyFlip <katzenberger.ph@gmail.com>
2023-01-18 21:09:30 +0100

    fix(module): Remove unnecessary modules from playground

--- module/playground/nuxt.config.ts
+++ module/playground/nuxt.config.ts
@@ -10,17 +10,6 @@ export default defineNuxtConfig({
         configPath: "~/tailwind.config.js",
       },
     ],
-    "unplugin-icons/nuxt",
-    [
-      "@nuxtjs/color-mode",
-      {
-        preference: "system", // default value of $colorMode.preference
-        fallback: "dark", // fallback value if not system preference found
-        classSuffix: "",
-        classPrefix: "",
-        storageKey: "theme",
-      },
-    ],
     "@vueuse/nuxt",
   ],
 });
