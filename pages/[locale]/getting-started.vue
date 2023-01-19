e389eb425ac7979303d407581734b1c84ac6bf80
FantasyFlip <katzenberger.ph@gmail.com>
2023-01-19 21:44:50 +0100

    feat(getting-started): Create page

---
+++ pages/[locale]/getting-started.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <NuxtLayout name="docs">
+      <div>Getting Started!</div>
+    </NuxtLayout>
+  </div>
+</template>
+
+<script setup>
+useHead({
+  title: "Getting Started",
+  meta: [
+    {
+      name: "description",
+      content: "Getting Started with Nuxtwind",
+    },
+  ],
+});
+definePageMeta({
+  layout: false,
+});
+</script>
