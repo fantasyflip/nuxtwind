7504afc1b06c6b91efcedfb0b5ca7938e000e5b4
FantasyFlip <katzenberger.ph@gmail.com>
2023-01-21 01:51:29 +0100

    docs(getting-started): Add translation

--- pages/[locale]/getting-started.vue
+++ pages/[locale]/getting-started.vue
@@ -2,33 +2,29 @@
   <div>
     <NuxtLayout name="docs">
       <NuxtLayout name="default">
-        <h1 class="text-5xl font-bold">Getting Started</h1>
+        <h1 class="text-5xl font-bold">
+          {{ $t("pages.gettingStarted.content.title") }}
+        </h1>
         <p class="py-6">
-          Using Nuxtwind is as easy as adding a module to your Nuxt.js project
-          and making minor changes to your TailwindCSS config.
+          {{ $t("pages.gettingStarted.content.description") }}
         </p>
         <div class="border-[0.5px] border-zinc-500 my-4"></div>
         <h2 class="text-4xl font-semibold py-4">Installation</h2>
-        <p class="py-3">
-          1. Add
-          <code class="dark:bg-zinc-800 bg-gray-200 rounded-lg p-1"
-            >@nuxtwind/components</code
-          >
-          dependency to your project:
-        </p>
+        <div
+          class="py-3"
+          v-html="
+            $t('pages.gettingStarted.content.installation.firstStep', {
+              package: '@nuxtwind/components',
+            })
+          "
+        ></div>
         <div class="py-3">
           <BashHighlight content="npm i @nuxtwind/components" />
         </div>
-        <p class="py-3">
-          2. Add it to your
-          <code class="dark:bg-zinc-800 bg-gray-200 rounded-lg p-1"
-            >modules</code
-          >
-          section in your
-          <code class="dark:bg-zinc-800 bg-gray-200 rounded-lg p-1"
-            >nuxt.config</code
-          >:
-        </p>
+        <div
+          class="py-3"
+          v-html="$t('pages.gettingStarted.content.installation.secondStep')"
+        ></div>
         <div class="py-3">
           <CodeHighlight
             content="export default defineNuxtConfig({
@@ -38,13 +34,10 @@
             path="nuxt.config.ts"
           />
         </div>
-        <p class="py-3">
-          3. Add the module path to the content array in your
-          <code class="dark:bg-zinc-800 bg-gray-200 rounded-lg p-1"
-            >tailwind.config</code
-          >. This is required for TailwindCSS to load all utility classes that
-          are used in Nuxtwind:
-        </p>
+        <div
+          class="py-3"
+          v-html="$t('pages.gettingStarted.content.installation.thirdStep')"
+        ></div>
         <div class="py-3">
           <CodeHighlight
             content='export default {
@@ -55,18 +48,16 @@
           />
         </div>
         <p class="py-3">
-          That's it! You can now use all Nuxtwind components in your project.
+          {{ $t("pages.gettingStarted.content.installation.done") }}
         </p>
         <div class="border-[0.5px] border-zinc-500 my-4"></div>
-        <h2 class="text-4xl font-semibold py-4">Options</h2>
-        <p class="py-3">
-          You can customize the module's behavior by providing available options
-          in the
-          <code class="dark:bg-zinc-800 bg-gray-200 rounded-lg p-1"
-            >modules</code
-          >
-          array in your nuxt.config:
-        </p>
+        <h2 class="text-4xl font-semibold py-4">
+          {{ $t("pages.gettingStarted.content.options.title") }}
+        </h2>
+        <div
+          class="py-3"
+          v-html="$t('pages.gettingStarted.content.options.description')"
+        ></div>
         <div class="py-3">
           <CodeHighlight
             content='export default defineNuxtConfig({
@@ -84,11 +75,13 @@
           />
         </div>
         <p class="py-3">
-          See the
+          {{ $t("pages.gettingStarted.content.options.link.prefix") }}
           <NuxtLink
             :to="useLocaleUrl('options')"
             class="text-primary-800 hover:text-secondary-700"
-            >module options</NuxtLink
+            >{{
+              $t("pages.gettingStarted.content.options.link.text")
+            }}</NuxtLink
           >.
         </p>
       </NuxtLayout>
