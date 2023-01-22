6a895addfd5f69147b0086bca739d4899a6e9ecd
FantasyFlip <katzenberger.ph@gmail.com>
2023-01-22 02:33:41 +0100

    docs(getting-started): Add caveat and meta

--- pages/[locale]/getting-started.vue
+++ pages/[locale]/getting-started.vue
@@ -43,7 +43,7 @@
         <div class="py-3">
           <CodeHighlight
             content='export default {
-  content: ["./module/src/runtime/components/**/*.vue"],
+  content: ["./node_modules/@nuxtwind/components/dist/runtime/components/*.vue"],
 };'
             lang="js"
             path="tailwind.config.js"
@@ -52,6 +52,19 @@
         <p class="py-3">
           {{ $t("pages.gettingStarted.content.installation.done") }}
         </p>
+        <div
+          class="w-full bg-yellow-300 border-2 border-yellow-400 rounded-lg p-2 text-black text-sm"
+        >
+          {{ $t("pages.gettingStarted.content.attention.theme.prefix") }}
+          <NuxtLink
+            :to="useLocaleUrl('theme')"
+            class="text-primary-800 hover:text-secondary-700 font-semibold"
+            >{{
+              $t("pages.gettingStarted.content.attention.theme.link")
+            }}</NuxtLink
+          >
+          {{ $t("pages.gettingStarted.content.attention.theme.suffix") }}
+        </div>
         <div class="border-[0.5px] border-zinc-500 my-4"></div>
         <h2 class="text-2xl md:text-3xl xl:text-4xl font-semibold py-4">
           {{ $t("pages.gettingStarted.content.options.title") }}
@@ -72,8 +85,8 @@
     ],
   ],
 });'
-            lang="js"
-            path="tailwind.config.js"
+            lang="ts"
+            path="nuxt.config.ts"
           />
         </div>
         <p class="py-3">
@@ -92,12 +105,59 @@
 </template>
 
 <script setup>
+import { useI18n } from "vue-i18n";
+const { t } = useI18n();
+let ogpImageUrl = useOgpImageUrl(
+  t("pages.gettingStarted.meta.title"),
+  t("pages.gettingStarted.meta.description"),
+  "components"
+);
 useHead({
-  title: "Getting Started",
+  title: t("pages.gettingStarted.meta.title"),
   meta: [
     {
+      hid: "description",
       name: "description",
-      content: "Getting Started with Nuxtwind",
+      content: t("pages.gettingStarted.meta.description"),
+    },
+    {
+      hid: "og:title",
+      property: "og:title",
+      content: t("pages.gettingStarted.meta.title"),
+    },
+    {
+      hid: "og:description",
+      property: "og:description",
+      content: t("pages.gettingStarted.meta.description"),
+    },
+    {
+      hid: "og:type",
+      property: "og:type",
+      content: "website",
+    },
+    {
+      hid: "og:image",
+      property: "og:image",
+      content: ogpImageUrl,
+    },
+    {
+      hid: "twitter:image",
+      property: "twitter:image",
+      content: ogpImageUrl,
+    },
+    {
+      hid: "twitter:card",
+      property: "twitter:card",
+      content: "summary_large_image",
+    },
+    {
+      hid: "og:url",
+      property: "og:url",
+      content: "https://nuxtwind.com/",
+    },
+    {
+      property: "og:locale",
+      content: t("pages.gettingStarted.meta.og.locale"),
     },
   ],
 });
