7a4a1d4b47a4b5c97086b8d9dd384408b98a813e
FantasyFlip <katzenberger.ph+github@gmail.com>
2023-01-24 21:13:27 +0100

    fix(examplecodedisplay): Add theme and adjust path dynamically

--- components/ExampleCodeDisplay.vue
+++ components/ExampleCodeDisplay.vue
@@ -3,7 +3,9 @@
     <div class="flex text-2xl pl-3">
       <div
         class="px-2 py-1 mr-3"
-        :class="showComponent ? 'bg-zinc-800 rounded-t-lg' : ''"
+        :class="
+          showComponent ? 'dark:bg-zinc-800 bg-gray-300 rounded-t-lg' : ''
+        "
       >
         <Button
           icon
@@ -23,7 +25,7 @@
       <div
         v-if="props.html"
         class="px-2 py-1 mr-3"
-        :class="showHtml ? 'bg-zinc-800 rounded-t-lg' : ''"
+        :class="showHtml ? 'dark:bg-zinc-800 bg-gray-300  rounded-t-lg' : ''"
       >
         <Button
           class="hover:-translate-y-px transition-all duration-300"
@@ -40,7 +42,9 @@
       <div
         v-if="props.js"
         class="px-2 py-1 mr-3"
-        :class="showJavascript ? 'bg-zinc-800 rounded-t-lg' : ''"
+        :class="
+          showJavascript ? 'dark:bg-zinc-800 bg-gray-300  rounded-t-lg' : ''
+        "
       >
         <Button
           class="hover:-translate-y-px transition-all duration-300"
@@ -57,18 +61,25 @@
     </div>
 
     <div
-      class="w-full h-[93%] grid place-items-center relative border-4 border-zinc-800 rounded-xl"
+      class="w-full h-[93%] grid place-items-center relative border-4 dark:border-zinc-800 border-gray-300 rounded-xl"
     >
       <transition name="fade">
-        <div v-if="showHtml && props.html" class="absolute">
-          <CodeHighlight
-            lang="html"
-            path="template"
-            content="<Button>Click me!</Button>"
-          />
+        <div v-if="showHtml && props.html" class="absolute w-full h-full">
+          <div
+            class="w-full h-full overflow-auto no-scrollbar grid place-items-center p-1"
+          >
+            <CodeHighlight lang="html" path="template" :content="props.html" />
+          </div>
         </div>
-        <div v-else-if="showJavascript && props.js" class="absolute">
-          <CodeHighlight lang="js" path="script" :content="props.js" />
+        <div
+          v-else-if="showJavascript && props.js"
+          class="absolute w-full h-full"
+        >
+          <div
+            class="w-full h-full overflow-auto no-scrollbar grid place-items-center p-1"
+          >
+            <CodeHighlight lang="js" path="script" :content="props.js" />
+          </div>
         </div>
         <div v-else-if="showComponent">
           <slot />
@@ -82,6 +93,9 @@
 import Button from "../module/src/runtime/components/Button.vue";
 import MdiLanguageHtml5 from "~icons/mdi/language-html5";
 import MdiLanguageJavascript from "~icons/mdi/language-javascript";
+
+const router = useRouter();
+const route = useRoute();
 
 const props = defineProps({
   html: {
@@ -99,21 +113,47 @@ let showComponent = ref(true);
 let showHtml = ref(false);
 let showJavascript = ref(false);
 
-const showTab = (tab) => {
+const changeQuery = (tab, keepHash = false) => {
+  if (route.hash && keepHash) {
+    router.replace({ query: { tab }, hash: route.hash });
+  } else {
+    router.replace({ query: { tab } });
+  }
+};
+
+const showTab = (tab, keepHash = false) => {
   if (tab === "component") {
     showComponent.value = true;
     showHtml.value = false;
     showJavascript.value = false;
+    changeQuery("component", keepHash);
   } else if (tab === "html") {
     showComponent.value = false;
     showHtml.value = true;
     showJavascript.value = false;
-  } else if (tab === "js") {
+    changeQuery("html", keepHash);
+  } else if (tab === "js" && props.js) {
     showComponent.value = false;
     showHtml.value = false;
     showJavascript.value = true;
+    changeQuery("js", keepHash);
+  } else {
+    showComponent.value = true;
+    showHtml.value = false;
+    showJavascript.value = false;
+    changeQuery("component", keepHash);
   }
 };
+
+onMounted(() => {
+  if (route.query.tab === "html") {
+    showTab("html", true);
+  } else if (route.query.tab === "js") {
+    showTab("js", true);
+  } else {
+    showTab("component", true);
+  }
+});
 </script>
 
 <style scoped>
@@ -130,5 +170,15 @@ const showTab = (tab) => {
 .fade-enter-to,
 .fade-leave-from {
   opacity: 1;
+}
+
+.no-scrollbar::-webkit-scrollbar {
+  display: none;
+}
+
+/* Hide scrollbar for IE, Edge and Firefox */
+.no-scrollbar {
+  -ms-overflow-style: none; /* IE and Edge */
+  scrollbar-width: none; /* Firefox */
 }
 </style>
