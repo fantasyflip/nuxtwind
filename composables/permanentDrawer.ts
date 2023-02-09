ae81318c5f816ec9b66b99317c06baf203ddde34
FantasyFlip <katzenberger.ph@gmail.com>
2023-02-09 04:06:17 +0100

    feat(permanentdrawer): Convert to TS

--- composables/permanentDrawer.ts
+++ composables/permanentDrawer.ts
@@ -1,10 +1,13 @@
 const permanentDrawer = ref(false);
 
-export const usePermanentDrawer = (newValue, updateLocalStorage = true) => {
+export const usePermanentDrawer = (
+  newValue: boolean,
+  updateLocalStorage = true
+) => {
   if (typeof newValue === "boolean") {
     permanentDrawer.value = newValue;
     if (updateLocalStorage) {
-      localStorage.setItem("permanentDrawer", newValue);
+      localStorage.setItem("permanentDrawer", newValue.toString());
     }
   }
   return permanentDrawer.value;
