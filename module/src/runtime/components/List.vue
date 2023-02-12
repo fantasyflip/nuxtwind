e54199bec969787bdaafc7a93c3a60e82c71ec2c
FantasyFlip <katzenberger.ph@gmail.com>
2023-02-12 22:08:36 +0100

    feat(list): Create component wrapper

---
+++ module/src/runtime/components/List.vue
@@ -0,0 +1,21 @@
+<template>
+  <div :class="wrapperClass">
+    <slot />
+  </div>
+</template>
+<script lang="ts" setup>
+export interface Props {
+  draggable?: boolean;
+}
+const props = withDefaults(defineProps<Props>(), {
+  draggable: false,
+});
+
+let wrapperClass = computed(() => {
+  let classes: string[] = [];
+  if (props.draggable) {
+    classes.push("draggable");
+  }
+  return classes.join(" ");
+});
+</script>
