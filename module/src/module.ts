821f2ee0d7d298ecfbecef345e6a009b6bfdf9ce
Philipp Katzenberger <katzenberger.ph+github@gmail.com>
2024-04-16 20:45:32 +0200

    refactor(module): improve module structure

--- module/src/module.ts
+++ module/src/module.ts
@@ -1,19 +1,60 @@
-import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
+import { defineNuxtModule, installModule, addComponentsDir, createResolver } from '@nuxt/kit'
+
+//@ts-ignore
+import { name, version } from "../package.json";
 
 // Module options TypeScript interface definition
-export interface ModuleOptions {}
+export interface ModuleOptions {
+  /**
+   * @default 'NXW-'
+   */
+  prefix?: string
+
+  /**
+   * @default false
+   */
+  global?: boolean
+}
 
 export default defineNuxtModule<ModuleOptions>({
   meta: {
-    name: 'my-module',
-    configKey: 'myModule',
+    name,
+    version,
+    configKey: "nuxtwind",
+    compatibility: {
+      nuxt: "^3.0.0",
+    },
   },
   // Default configuration options of the Nuxt module
-  defaults: {},
-  setup(_options, _nuxt) {
+  defaults: {
+    prefix: 'NXW-',
+    global: false
+  },
+  async setup(_options, _nuxt) {
+    //@ts-ignore
     const resolver = createResolver(import.meta.url)
+    const runtimeDir = resolver.resolve('./runtime')
 
-    // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
-    addPlugin(resolver.resolve('./runtime/plugin'))
+    _nuxt.options.build.transpile.push(runtimeDir);
+
+    //components
+    addComponentsDir({
+      prefix: _options.prefix,
+      path: resolver.resolve(runtimeDir, 'components'),
+      global: _options.global
+    })
+
+    await installModule('@nuxtjs/tailwindcss', {
+      // module configuration
+      exposeConfig: true,
+      config: {
+        content: {
+          files: [
+            resolver.resolve('./runtime/components/**/*.{vue,mjs,ts}'),
+            resolver.resolve('./runtime/*.{mjs,js,ts}')
+          ]
+        }
+      }
+    })
   },
 })
