9a6040b7c595c1c7b5096ef3c1d72ce657b8bda1
BLUEBEAST\katze <katzenberger.ph+github@gmail.com>
2025-07-18 23:42:03 +0200

    feat(module): add default theme colors

--- playground/app/app.vue
+++ playground/app/app.vue
@@ -1,8 +1,13 @@
 <template>
   <div>
     <NXW-Button>Button</NXW-Button>
+    <NXW-Progress
+      v-model="loadingProgress"
+      :loading="true"
+    />
   </div>
 </template>
 
 <script setup>
+const loadingProgress = ref(33)
 </script>
