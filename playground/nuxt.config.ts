a864ee875be69754f7919db944cfd8c5f8b61d21
FantasyFlip <>
2025-12-02 18:24:54 +0100

    chore(playground): remove tarball reference

--- playground/nuxt.config.ts
+++ playground/nuxt.config.ts
@@ -1,4 +1,5 @@
 export default defineNuxtConfig({
+  // TODO run pnpm add "generated-tarball" before starting dev server
   modules: ['@nuxtwind/components'],
   devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
